ee6a428d56ab6ef4f6e252a6b911f8cf6f52c265
Sai Swaroop <saiswaroop874@gmail.com>
2023-01-22 18:56:39 +0300

    init

--- example3.js
+++ example3.js
@@ -24,3 +24,12 @@ console.log(0.1 + 0.2 == 1.3 );
 //       return Math.abs(num1 - num2) < Number.EPSILON;
 // }
 // console.log(areTheNumbersAlmostEqual(0.1 + 0.2, 0.3));
+
+
+
+console.log(1 + "2" + "2");
+console.log(1 + +"2" + "2");
+console.log(1 + -"1" + "2");
+console.log(+"1" + "1" + "2");
+console.log("A" - "B" + "2");
+console.log("A" - "B" + 2);
--- example6.js
+++ example6.js
@@ -2,15 +2,12 @@ var arr1 = "john".split('');
 var arr2 = arr1.reverse();
 var arr3 = "jones".split('');
 arr2.push(arr3);
-console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1));
+console.log("array 1: length=" + arr1.length + " last=" + arr1);
 console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));
+console.log("array 2: length=" + arr2.length + " last=" + arr2.slice());
 
 
-
-
-console.log(1 + "2" + "2");
-console.log(1 + +"2" + "2");
-console.log(1 + -"1" + "2");
-console.log(+"1" + "1" + "2");
-console.log("A" - "B" + "2");
-console.log("A" - "B" + 2);
+// arr1 = J,o,h,n
+// arr2 = n,h,o,j 
+// arr3 = j,o,n,e,s 
+// [n,h,o,j , [ j,o,n,e,s]]
