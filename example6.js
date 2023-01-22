cce47ac733e3c8f113bfe5b7672eb3fcf6103877
Sai Swaroop <saiswaroop874@gmail.com>
2023-01-22 14:29:04 +0300

    init

--- example6.js
+++ example6.js
@@ -4,3 +4,13 @@ var arr3 = "jones".split('');
 arr2.push(arr3);
 console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1));
 console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));
+
+
+
+
+console.log(1 + "2" + "2");
+console.log(1 + +"2" + "2");
+console.log(1 + -"1" + "2");
+console.log(+"1" + "1" + "2");
+console.log("A" - "B" + "2");
+console.log("A" - "B" + 2);
