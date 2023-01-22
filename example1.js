6fbed7ea02c21b883dd78663ddd4eb80da4ca8dc
Sai Swaroop <saiswaroop874@gmail.com>
2023-01-22 13:55:23 +0300

    init

--- example1.js
+++ example1.js
@@ -1,6 +1,17 @@
+// (function () {
+//       var a = b = 3;
+// })();
+
+// var a = b = 3;  // var a = b and b = 3 , var a = (b = 3) 
+
 (function () {
       var a = b = 3;
 })();
 
+
 console.log("a defined? " + (typeof a !== 'undefined'));
 console.log("b defined? " + (typeof b !== 'undefined'));
+console.log(typeof a);
+console.log(typeof b);
+console.log(b);
+console.log(a);  // a is not defined here
